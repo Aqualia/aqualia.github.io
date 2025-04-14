103005693506009c3f45f033b6d64cd83c71d31f
Bhatte <64166046+Bhatte@users.noreply.github.com>
2025-04-14 11:21:51 +0100

    App routing changes to host on GitHub

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/aqualia.github.io/', // <-- Replace <repository-name>
+  base: '/', // <-- Replace <repository-name>
 })
